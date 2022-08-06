4716dc4923cd380130d21cdb496d0dd29499dde1
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-08-05 21:37:25 -0600

    Added more special characters

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ generateBtn.addEventListener("click", writePassword);
 
 //character arrays in global memory
 var special = [
-  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"
+  "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "[", "]", "_", "{", "}", "|", "~"
 ]
 var numeric = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9
